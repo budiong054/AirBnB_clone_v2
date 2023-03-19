bcd94c21b17c4516c7f45e2bad747f4de8fcbdfa
Blessing Udiong <udiongblessing@gmail.com>
2023-03-19 21:48:07 +0000

    Revert all changes

--- models/city.py
+++ models/city.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
     state_id = ""
-    name = Column(string(128), nullable=False)
-    state_id = Column(string(60), Foreignkey('state.id'),
-                       nullable=False)
-    places = relationship("Place", cascade="delete", backref='cities')  
+    name = ""
--- models/user.py
+++ models/user.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Foreignkey
-from sqlalchemy.orm import relationship
-
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = 'user'
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=False)
-    last_name = Column('first_name', String(128), nullable=False)
-    #this was implemented in task 8
-    places = relationship("place", cascade="delete", backref="user")
-    #for caution
-    revives = relationship("review", cascade="delete", backref="user")
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
