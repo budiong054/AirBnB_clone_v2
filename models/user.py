873b07db017a54af16a8c577a6e1885301069495
FORMAT1 <olayiwolafm@gmail.com>
2023-03-19 12:40:49 +0000

    models

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Foreignkey
+from sqlalchemy.orm import relationship
+
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'user'
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=False)
+    last_name = Column('first_name', String(128), nullable=False)
+    #this was implemented in task 8
+    places = relationship("place", cascade="delete", backref="user")
+    #for caution
+    revives = relationship("review", cascade="delete", backref="user")
