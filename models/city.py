66ad3a4f236c43c2b0a557eb80b98d861ba5c395
FORMAT1 <olayiwolafm@gmail.com>
2023-03-19 11:11:06 +0000

    dbstorage and updating model state and city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     state_id = ""
-    name = ""
+    name = Column(string(128), nullable=False)
+    state_id = Column(string(60), Foreignkey('state.id'),
+                       nullable=False)
+    places = relationship("Place", cascade="delete", backref='cities')  
--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Foreignkey
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
+import os
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'state'
+    name = Column(String(128), nullable=false)
+    cities = relationship("city", cascade="delete", backref="state")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def cities(self):
+            """cities getter attribute"""
+        cit_lis = []
+        all_cit = models.storage.all(city)
+        for city in all_cit.values():
+            if city.state_id == self.id:
+                cit_lis.append(city)
+        return cit_lis
