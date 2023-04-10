aa6f918403e2e8e3b669caa0b0d2d34a114c4b2e
Blessing Udiong <udiongblessing@gmail.com>
2023-04-10 11:31:13 +0000

    Refactor script to fix 404 error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,44 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web static
-sudo apt-get update
 
-sudo apt-get install nginx -y
+apt-get update
+apt-get install nginx -y
 
 DIR_1=/data/web_static/releases/test/
 DIR_2=/data/web_static/shared/
 SYMLINK_FILE=/data/web_static/current
 html_content="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
 
-sudo mkdir -p $DIR_1
-
-sudo mkdir -p $DIR_2
-
+mkdir -p $DIR_1
+mkdir -p $DIR_2
 echo -e "$html_content" > "$DIR_1"index.html
 
 if [ -L $SYMLINK_FILE ]
 then
-        sudo unlink $SYMLINK_FILE
+        unlink $SYMLINK_FILE
 fi
 
-sudo ln -s $DIR_1 $SYMLINK_FILE
+ln -s $DIR_1 $SYMLINK_FILE
+chown -R ubuntu:ubuntu /data/
 
-sudo chown -R ubuntu:ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-enabled/default
 
-new_config="server_name \_\;\n\tlocation \/hbnb_static \{\n\t\talias \/data\/web\_static\/current\/\;\n\t\}\n"
-
-sed -i "s/server_name _;/$new_config/" /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
+service nginx restart
