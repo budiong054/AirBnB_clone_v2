ca134f22b19188b36482f0ea3d48be5421c2894e
Blessing Udiong <udiongblessing@gmail.com>
2023-04-10 10:59:38 +0000

    Refactor do_pack function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,25 +4,28 @@
 """
 from fabric.api import local
 from datetime import datetime
-from os import mkdir
-
-
-date = datetime.now()
-tgz_file = f"web_static_{date.year}{date.month}{date.day}{date.hour}"\
-    f"{date.minute}{date.second}.tgz"
+import os
 
 
 def do_pack():
     """pack all the content of the web_static folder to a tgz file
     """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    archive_path = "versions/web_static_{}{:02d}{:02d}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
-        mkdir("versions")
-    except FileExistsError:
-        pass
-    
-    # Packing web_static to versions/{tgz_file}
-    pack = local(f"tar -cvzf versions/{tgz_file} web_static")
-
-    if pack.succeeded:
-        return f"versions/{tgz_file}"
-    return None
+        print("Packing web_static to {}".format(archive_path))
+        local("tar -cvzf {} web_static".format(archive_path))
+        size = os.stat(archive_path).st_size
+        print("web_static packed: {} -> {} Bytes".format(archive_path, size))
+    except Exception:
+        archive_path = None
+    return archive_path
