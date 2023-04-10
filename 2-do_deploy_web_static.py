5bbdb1c336f9c6997450b9a291fe1186c891e46b
Blessing Udiong <udiongblessing@gmail.com>
2023-04-10 12:03:37 +0000

    Refactor deploy to meet all requirement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,30 @@ from os import path
 env.hosts = ['18.233.67.42', '54.237.66.155']
 
 
+def do_pack():
+    """pack all the content of the web_static folder to a tgz file
+    """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    archive_path = "versions/web_static_{}{:02d}{:02d}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(archive_path))
+        local("tar -cvzf {} web_static".format(archive_path))
+        size = os.stat(archive_path).st_size
+        print("web_static packed: {} -> {} Bytes".format(archive_path, size))
+    except Exception:
+        archive_path = None
+    return archive_path
+
+
 def do_deploy(archive_path):
     """This scripts distributes an archive to your webservers
     """
